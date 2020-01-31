fda83bc483023ab33a5d96c6a16d088e46fbab08
Ramon Carceles <juanramoncarceles@gmail.com>
2020-01-31 10:25:28 +0100

    First commit.

---
+++ VisualARQAdvancedSelector/SelectionDialog.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eto.Drawing;
+using Eto.Forms;
+
+namespace VisualARQAdvancedSelector
+{
+    public class SelectionDialog : Dialog<bool>
+    {
+        public SelectionDialog()
+        {
+            // Dialog box initialization
+            Title = "Select objects by parameter";
+            Resizable = false;
+
+            // Default button
+            DefaultButton = new Button { Text = "Select" };
+            DefaultButton.Click += OnSelectButtonClick;
+
+            // Abort button
+            AbortButton = new Button { Text = "Cancel" };
+            AbortButton.Click += OnCloseButtonClick;
+
+            // Table layout to add all the controls
+            DynamicLayout layout = new DynamicLayout
+            {
+                Spacing = new Size(15, 15),
+                Padding = new Padding(10)
+            };
+            layout.BeginVertical();
+            layout.AddRow(Param_name_label);
+            layout.AddRow(Param_name_textbox);
+            layout.EndVertical();
+            layout.BeginVertical();
+            layout.AddRow(param_value_label);
+            layout.AddRow(Comparison_value, Param_value_textbox);
+            layout.EndVertical();
+            layout.BeginVertical();
+            layout.AddRow(Add_to_selection_checkbox);
+            layout.EndVertical();
+            layout.AddSeparateRow(null, DefaultButton, null, AbortButton, null);
+            Content = layout;
+        }
+
+        // Parameter name input
+        private Label Param_name_label = new Label
+        {
+            Text = "Parameter name:"
+        };
+        private TextBox Param_name_textbox = new TextBox(); // O poner Text=null
+
+        // Comparison option dropdown
+        private DropDown Comparison_value = new DropDown
+        {
+            DataStore = new string[3] { "is equal to", "is less than", "is greater than" },
+            SelectedIndex = 0
+        };
+
+        // Parameter value input
+        private Label param_value_label = new Label
+        {
+            Text = "Parameter value:"
+        };
+        private TextBox Param_value_textbox = new TextBox(); // O poner Text=null
+
+        // Add to current selection input
+        private CheckBox Add_to_selection_checkbox = new CheckBox
+        {
+            Text = "Add to current selection",
+            Checked = true
+        };
+
+
+
+
+
+
+        // Get the parameter name
+        public string GetParamName()
+        {
+            return Param_name_textbox.Text;
+        }
+
+        // Get the type of comparison
+        private int GetComparisonType()
+        {
+            return Comparison_value.SelectedIndex;
+        }
+
+        // Get the value of the parameter
+        public string GetParamValue()
+        {
+            return Param_value_textbox.Text;
+        }
+
+        // Get the add to current selection checkbox
+        private bool? GetAddToSelection()
+        {
+            return Add_to_selection_checkbox.Checked;
+        }
+        
+        // Close button click handler
+        private void OnCloseButtonClick<TEventArgs>(object sender, TEventArgs e)
+        {
+            Param_name_textbox.Text = "";
+            // TODO Clean all the inputs
+            Close(false);
+        }
+
+        // Select button click handler
+        private void OnSelectButtonClick<TEventArgs>(object sender, TEventArgs e)
+        {
+            if (Param_name_textbox.Text == "")
+            {
+                Close(false);
+            }
+            else
+            {
+                Close(true);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ VisualARQAdvancedSelector/VisualARQAdvancedSelectorCommand.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace VisualARQAdvancedSelector
+{
+    public class VisualARQAdvancedSelectorCommand : Command
+    {
+        public VisualARQAdvancedSelectorCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static VisualARQAdvancedSelectorCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "VisualARQAdvancedSelectorCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO Here the main logic...
+
+            SelectionDialog sd = new SelectionDialog();
+
+            bool rc = sd.ShowModal(Rhino.UI.RhinoEtoApp.MainWindow);
+
+            if (rc)
+            {
+                // TODO Filter also by type of obj?
+                // rhobjs = filter( lambda x: va.IsColumn(x.Id) , Rhino.RhinoDoc.ActiveDoc.Objects)
+
+                Rhino.DocObjects.Tables.ObjectTable rhobjs = RhinoDoc.ActiveDoc.Objects;
+
+                string paramName = sd.GetParamName();
+
+                if (paramName != "")
+                {
+                    // List to store all the objects that match.
+                    List<Rhino.DocObjects.RhinoObject> matched = new List<Rhino.DocObjects.RhinoObject>();
+
+                    string paramValue = sd.GetParamValue();
+
+                    if (paramValue != "") // Search by name and value.
+                    {
+                        // TODO...
+
+                        RhinoApp.WriteLine("Search by name and value.");
+
+                    }
+                    else // Search only by name.
+                    {
+                        // TODO...
+
+                        RhinoApp.WriteLine("Search only by name.");
+
+                    }
+
+                    // Select all the ones that matched.
+                    if (matched.Count > 0)
+                    {
+                        // TODO...
+                        RhinoApp.WriteLine("Selcting...");
+                    }
+                }
+            }
+
+
+
+
+            // Example code
+            //Point3d pt0;
+            //using (GetPoint getPointAction = new GetPoint())
+            //{
+            //    getPointAction.SetCommandPrompt("Please select the start point");
+            //    if (getPointAction.Get() != GetResult.Point)
+            //    {
+            //        RhinoApp.WriteLine("No start point was selected.");
+            //        return getPointAction.CommandResult();
+            //    }
+            //    pt0 = getPointAction.Point();
+            //}
+
+            return Result.Success;
+        }
+    }
+}
