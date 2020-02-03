4d3dcefdbf764fc5d00a65000d3787974af63749
Ramon Carceles <juanramoncarceles@gmail.com>
2020-02-03 21:18:28 +0100

    First step on openings selection filter.

---
+++ VisualARQAdvancedSelector/OpeningsFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static VisualARQ.Script;
+
+namespace VisualARQAdvancedSelector
+{
+    public class OpeningsFilter
+    {
+        private void Filter()
+        {
+            // Window, door or both
+            //VisualARQ.Script.IsOpening();
+            //VisualARQ.Script.IsWindow();
+            //VisualARQ.Script.IsDoor();
+
+
+            // style
+            // show all the styles available
+            //VisualARQ.Script.GetAllWindowStyleIds();
+            //VisualARQ.Script.GetAllDoorStyleIds();
+
+            // profile type
+            // show all the profiles available
+            //VisualARQ.Script.IsOpeningProfile();
+            //VisualARQ.Script.GetProfileTemplates();
+
+            // profile dimensions
+            //VisualARQ.Script.GetOpeningProfile()
+            
+            //VisualARQ.Script.GetProductStyle()
+
+            
+        }
+    }
+}
